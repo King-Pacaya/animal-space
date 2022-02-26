f5cf582438eb19aa2f605c7138d9adf4002a5b5d
King Pacaya <83373501+King-Pacaya@users.noreply.github.com>
2022-02-26 10:04:43 -0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,31 @@
+//Nav
+	//Responsive Dropdown
+		function myFunction() {
+		  var x = document.getElementById("myTopnav");
+		  if (x.className === "topnav") {
+		    x.className += " responsive";
+		  } else {
+		    x.className = "topnav";
+		  }
+		}
+	//Scroll
+	var prevScrollpos = window.pageYOffset;
+	window.onscroll = function() {
+	var currentScrollPos = window.pageYOffset;
+	  if (prevScrollpos > currentScrollPos) {
+	    document.getElementById("myTopnav").style.top = "0";
+	  } else {
+	    document.getElementById("myTopnav").style.top = "-15vh";
+	  }
+	  prevScrollpos = currentScrollPos;
+	}
+	// adding click events
+	var els = document.querySelectorAll('a');
+	[].forEach.call(els, function(el) {
+	  el.addEventListener('click', function() {
+	    [].forEach.call(els, function(clk) {
+	      clk.classList.remove('active');
+	    });
+	    el.classList.add('active');
+	  });
+	});
