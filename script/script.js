e2af6f2abfef5a13335ac80fdea46f0e91aa215a
King Pacaya <83373501+King-Pacaya@users.noreply.github.com>
2022-06-06 01:12:30 -0500

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -29,3 +29,19 @@
 	    el.classList.add('active');
 	  });
 	});
+
+//Accordion
+var acc = document.getElementsByClassName("gtitulo");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("gactive");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
